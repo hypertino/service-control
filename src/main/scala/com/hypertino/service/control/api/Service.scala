cc0c2eebe7a3127995bc1eb3d45d87771cfa3622
Maga Abdurakhmanov <maqdev@gmail.com>
2017-12-21 13:36:07 +0100

    * default onServiceStart which isn't required to override

--- src/main/scala/com/hypertino/service/control/api/Service.scala
+++ src/main/scala/com/hypertino/service/control/api/Service.scala
@@ -4,6 +4,6 @@ import scala.concurrent.Future
 import scala.concurrent.duration.FiniteDuration
 
 trait Service {
-  def startService(): Unit
+  def startService(): Unit = {} // often we don't need separate initialize/start
   def stopService(controlBreak: Boolean, timeout: FiniteDuration): Future[Unit]
 }
